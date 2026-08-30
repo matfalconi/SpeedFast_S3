fe15aae77c93fb95cffc7cdf6f47ae445e6da6a5
matfalconi <ma.falconi@duocuc.cl>
2026-08-30 02:59:42 -0400

    Delete SpeedFast_S3 directory

--- SpeedFast_S3/src/Speedfast/interfaces/Despachable.java
+++
@@ -1,6 +0,0 @@
-package Speedfast.interfaces;
-
-public interface Despachable {
-    //pone pedido en ruta
-    void despachar();
-}
--- SpeedFast_S3/src/Speedfast/interfaces/Rastreable.java
+++
@@ -1,6 +0,0 @@
-package Speedfast.interfaces;
-
-public interface Rastreable {
-    //Muestra el historial de eventos del pedido.
-    void verHistorial();
-}
